d1f7ff462ace97abb4b3a27a48026d8cd6c44e1f
irfannfauzan <irfanfauzanrachman@gmail.com>
2020-11-26 16:24:24 +0700

    iOS apps

--- CalculatorV1/ViewController.swift
+++ CalculatorV1/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var angkaPertama: UITextField!
+    @IBOutlet weak var angkaKedua: UITextField!
+    @IBOutlet weak var resultHasil: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resultHasil.text="Halo"
     }
 
-
+    @IBAction func tombolHitung(_ sender: Any) {
+        let angka1 = intmax_t(angkaPertama.text!)!
+        let angka2 = intmax_t(angkaKedua.text!)!
+        let hasil = angka1 + angka2
+        resultHasil.text="\(hasil)"
+    }
+    
+    @IBAction func Hitung(_ sender: UIButton) {
+        angkaPertama.text=""
+        angkaKedua.text=""
+        resultHasil.text=""
+    }
+    
+    
+    @IBAction func tombolKurang(_ sender: Any) {
+        let angka1 = intmax_t(angkaPertama.text!)!
+        let angka2 = intmax_t(angkaKedua.text!)!
+        let hasil = angka1 - angka2
+        resultHasil.text="\(hasil)"
+    
+    }
+    
+    
+    @IBAction func tombolKali(_ sender: UIButton) {
+        let angka1 = intmax_t(angkaPertama.text!)!
+        let angka2 = intmax_t(angkaKedua.text!)!
+        let hasil = angka1 * angka2
+        resultHasil.text="\(hasil)"
+    
+    }
+    
+    @IBAction func reset(_ sender: UIButton) {
+        angkaPertama.text=""
+        angkaKedua.text=""
+        resultHasil.text=""
+    }
 }
 
